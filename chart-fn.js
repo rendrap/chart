a0765e3508fe8d839286734b2edd88196e132bdc
rendrap <rendrapradana@gmail.com>
2019-12-09 15:30:18 +0700

    change chart.js to local and modify the source to remove bug about skip view

--- chart-fn.js
+++ chart-fn.js
@@ -142,4 +142,121 @@ xhttp.onreadystatechange = function () {
 xhttp.open("GET", "https://forbes400.herokuapp.com/api/forbes400?limit=10", false);
 xhttp.send();
 
+// --------------------------
 
+var chartColors = {
+  red: 'rgb(255, 99, 132)',
+  orange: 'rgb(255, 159, 64)',
+  yellow: 'rgb(255, 205, 86)',
+  green: 'rgb(75, 192, 192)',
+  blue: 'rgb(54, 162, 235)',
+  purple: 'rgb(153, 102, 255)',
+  grey: 'rgb(201, 203, 207)'
+};
+
+function randomScalingFactor() {
+  return (Math.random() > 0.5 ? 1.0 : -1.0) * Math.round(Math.random() * 100);
+}
+
+// Only works from if the html is served on localhost:3000
+function onRefresh(chart) {
+  $.ajax({
+    url: 'http://pixelprowess.com/i/stream.php',
+    dataType: 'json',
+    async: true,
+    crossDomain: true,
+    method : "GET",
+    success: function(data) {
+      chart.config.data.datasets[0].data.push({
+        x: Date.now(),
+        // x: moment(data.time_stamp,'HH:mm:ss'),
+        y: data.red_value
+      })
+      chart.config.data.datasets[1].data.push({
+        x: Date.now(),
+        // x: moment(data.time_stamp,'HH:mm:ss'),
+        y: data.blue_value
+      })
+      // console.log(chart.config.data.datasets[1].data);
+
+    }
+  });
+}
+
+// Dummy random data
+// function onRefresh(chart) {
+//   chart.config.data.datasets.forEach(function(dataset) {
+//     dataset.data.push({
+//       x: Date.now(),
+//       y: randomScalingFactor()
+//     });
+//   });
+// }
+//
+
+
+var color = Chart.helpers.color;
+var config = {
+  type: 'line',
+  data: {
+    datasets: [{
+      label: 'Dataset 1 (linear interpolation)',
+      backgroundColor: color(chartColors.red).alpha(0.5).rgbString(),
+      borderColor: chartColors.red,
+      fill: false,
+      lineTension: 0,
+      borderDash: [8, 4],
+      data: []
+    }, {
+      label: 'Dataset 2 (cubic interpolation)',
+      backgroundColor: color(chartColors.blue).alpha(0.5).rgbString(),
+      borderColor: chartColors.blue,
+      fill: false,
+      cubicInterpolationMode: 'monotone',
+      data: []
+    }]
+  },
+  options: {
+    title: {
+      display: true,
+      text: 'Line chart (hotizontal scroll) sample'
+    },
+    scales: {
+      xAxes: [{
+        type: 'realtime',
+        realtime: {
+          duration: 20000,
+          refresh: 1000,
+          delay: 2000,
+          onRefresh: onRefresh
+        }
+      }],
+      yAxes: [{
+        scaleLabel: {
+          display: true,
+          labelString: 'value'
+        }
+      }]
+    },
+    tooltips: {
+      mode: 'nearest',
+      intersect: false,
+      // enabled: false
+    },
+    hover: {
+      mode: 'nearest',
+      intersect: false,
+      // animationDuration: 1
+    },
+    plugins: {
+      streaming: {            // per-chart option
+          frameRate: 30       // chart is drawn 30 times every second
+      }
+    }
+  }
+};
+
+window.onload = function() {
+  var ctx = document.getElementById('dynChart').getContext('2d');
+  window.myChart = new Chart(ctx, config);
+};
